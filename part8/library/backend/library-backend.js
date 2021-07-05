e1b1fa6077e91ebc338589661385ddc24015d12c
ndinhphan <ndinhphan@gmail.com>
2021-07-05 17:37:51 +0700

    end of ex 8.7

---
+++ part8/library/backend/library-backend.js
@@ -0,0 +1,166 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { v1: uuid } = require("uuid");
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+];
+
+/*
+ * English:
+ * It might make more sense to associate a book with its author by storing the author's name in the context of the book instead of the author's id
+ * However, for simplicity, we will store the author's name in connection with the book
+ */
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "The Demon ",
+    published: 1872,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+];
+
+const typeDefs = gql`
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
+`;
+
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+      let result = books;
+      if (args.author)
+        result = books.filter((book) => book.author === args.author);
+      if (args.genre) {
+        result = result.filter((result) => result.genres.includes(args.genre));
+      }
+      return result;
+    },
+    allAuthors: (root, args) => {
+      if (args.name) {
+        return authors.find((p) => p.name === args.name);
+      }
+      return authors;
+    },
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      books.concat({
+        title: args.title,
+        published: args.published,
+        author: args.author,
+        genres: args.genres,
+        id: uuid(),
+      });
+      return books[books.length - 1];
+    },
+    editAuthor: (root,args) => {
+      const findAuthor = authors.find(author => author.name === args.name)
+      if (!findAuthor) return null
+      const editedAuthor = {...findAuthor, born: args.setBornTo}
+      const newauthors = authors.map(author => author.name === args.name ? editedAuthor : author)
+      if(findAuthor) return editedAuthor 
+    }
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
